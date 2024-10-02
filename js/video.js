c2cc6b17a00a82bc8f38dbeb21e2557d93f0c9ac
Noman <nomanmia128@gmail.com>
2024-10-03 00:47:23 +0600

    added videos

--- js/video.js
+++ js/video.js
@@ -8,8 +8,60 @@ const loadCategories = () => {
     .then((res) => res.json())
     .then((data) => displayCategories(data.categories))
     .catch((error) => console.log(error));
-
 };
+const loadVideos = () => {
+    // fetch the data
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
+
+const cardDemo ={
+    
+        "category_id": "1003",
+        "video_id": "aaae",
+        "thumbnail": "https://i.ibb.co/Yc4p5gD/inside-amy.jpg",
+        "title": "Inside Amy Schumer",
+        "authors": [
+            {
+                "profile_picture": "https://i.ibb.co/YD2mqH7/amy.jpg",
+                "profile_name": "Amy Schumer",
+                "verified": ""
+            }
+        ],
+        "others": {
+            "views": "3.6K",
+            "posted_date": "15147"
+        },
+        "description": "'Inside Amy Schumer' is a comedy show by the popular comedian Amy Schumer, blending sharp satire and unfiltered humor to tackle everyday issues and societal norms. With 3.6K views, the show promises a blend of hilarious sketches, thought-provoking stand-up, and candid interviews. It's a must-watch for fans of bold, edgy comedy."
+    
+}
+
+const displayVideos = (videos) => {
+   const videoContainer = document.getElementById('videos')
+   videos.forEach((video) => {
+    console.log(video);
+    const card = document.createElement('div');
+    card.classList = 'card card-compact';
+    card.innerHTML = 
+    `
+    <figure>
+    <img
+      src= ${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+    `;
+    videoContainer.append(card);
+   })
+}
 
 // Create DisplyCategories
 const displayCategories = (categories) => {
@@ -28,3 +80,4 @@ const displayCategories = (categories) => {
 };
 
 loadCategories();
+loadVideos();
